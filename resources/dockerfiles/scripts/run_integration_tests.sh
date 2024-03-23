b031e3031967d871eb3db2600434454368de7358
Thibault Cheneviere <thibault.chene23@gmail.com>
2024-03-23 12:36:26 -0400

    feat: fixed integration test script

--- resources/dockerfiles/scripts/run_integration_tests.sh
+++ resources/dockerfiles/scripts/run_integration_tests.sh
@@ -10,7 +10,7 @@ export REGISTRY_PASSWORD=$(date | md5sum | cut -f1 -d\ )
 docker create -v /etc/docker/registry --name config alpine:3 /bin/true
 echo -n "${REGISTRY_PASSWORD}" | docker run --rm -i --entrypoint=htpasswd --volumes-from config thibaultcne/htpasswd -i -B -c /etc/docker/registry/htpasswd shiprs
 cat resources/dockerfiles/registry/config.yml | docker run --rm -i --volumes-from config --entrypoint=tee alpine:3 /etc/docker/registry/config.yml
-docker run -d --restart always --name registry -p 8080:5000 --volumes-from config registry:2
+docker run -d --restart always --name registry -p 5000:5000 --volumes-from config registry:2
 sleep 4
 
 # Pull images from the docker.io registry
@@ -19,18 +19,18 @@ docker pull alpine
 docker pull androw/uhttpd
 
 # Tag images to then push them to the local registry
-docker tag hello-world:linux localhost:8080/hello-world:linux
-docker tag alpine localhost:8080/alpine
-docker tag httpd localhost:8080/androw/uhttpd
+docker tag hello-world:linux localhost:5000/hello-world:linux
+docker tag alpine localhost:5000/alpine
+docker tag androw/uhttpd localhost:5000/androw/uhttpd
 
 # Login to registry
-echo -n "${REGISTRY_PASSWORD}" | docker login --username shiprs --password-stdin localhost:8080
+docker login --username shiprs --password "${REGISTRY_PASSWORD}" localhost:5000
 
 # Push images to the local registry
-docker push localhost:8080/hello-world:linux
-docker push localhost:8080/alpine
-docker push localhost:8080/androw/uhttpd
+docker push localhost:5000/hello-world:linux
+docker push localhost:5000/alpine
+docker push localhost:5000/androw/uhttpd
 
 # Run tests
 docker swarm init
-docker run -e REGISTRY_PASSWORD -e REGISTRY_HTTP_ADDR=localhost:8080 -v /var/run/docker.sock:/var/run/docker.sock -ti --rm shiprs cargo test $@ -- --test-threads 1
+docker run -e REGISTRY_PASSWORD -e REGISTRY_HTTP_ADDR=localhost:5000 -v /var/run/docker.sock:/var/run/docker.sock -ti --rm shiprs cargo test $@ -- --test-threads 1
